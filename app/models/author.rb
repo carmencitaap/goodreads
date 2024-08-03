1e353e4a180cf1eff047587eae958ac9ec91e265
carmencitaap <cmavendano@miuandes.cl>
2024-08-02 20:24:03 -0400

    first commit

---
+++ app/models/author.rb
@@ -0,0 +1,8 @@
+class Author
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :name, type: String
+  field :date_of_birth, type: Date
+  field :country_of_origin, type: String
+  field :short_description, type: String
+end
